f576a706d6406bd46311a8fd6f0d367025860e12
Arnaud Ligny <arnaud@ligny.fr>
2022-11-22 23:05:20 +0100

    Update cecil.rb

--- Formula/cecil.rb
+++ Formula/cecil.rb
@@ -3,8 +3,15 @@ class Cecil < Formula
   homepage "https://cecil.app"
   url "https://github.com/Cecilapp/Cecil/releases/download/7.27.6/cecil.phar"
   sha256 "921d330adf031aeeb1b13791f4dc8585685c62e375e260989f62f032b22ddc17"
+  license "MIT"
+  
+  livecheck do
+    url "https://cecil.app/cecil.phar"
+    https://cecil.app/download/4.0.0/cecil.phar
+    regex(%r{/download/(\d+(?:\.\d+)+)/cecil\.phar}i)
+  end
 
-  depends_on "php" if MacOS.version <= :el_capitan
+  depends_on "php"
 
   def install
     bin.install "cecil.phar" => "cecil"
