bfe5b90af980c9e4d251845ec182022588ec66e8
Arnaud Ligny <arnaud@ligny.fr>
2022-11-22 23:07:45 +0100

    Update cecil.rb

--- Formula/cecil.rb
+++ Formula/cecil.rb
@@ -4,12 +4,6 @@ class Cecil < Formula
   url "https://github.com/Cecilapp/Cecil/releases/download/7.27.6/cecil.phar"
   sha256 "921d330adf031aeeb1b13791f4dc8585685c62e375e260989f62f032b22ddc17"
   license "MIT"
-  
-  livecheck do
-    url "https://cecil.app/cecil.phar"
-    https://cecil.app/download/4.0.0/cecil.phar
-    regex(%r{/download/(\d+(?:\.\d+)+)/cecil\.phar}i)
-  end
 
   depends_on "php"
 
