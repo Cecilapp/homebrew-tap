a65c260f04beba2344e158b6aca3bff25ff4657c
Arnaud Ligny <arnaud@ligny.fr>
2023-03-12 23:47:39 +0100

    cecil 7.32.4 (#25)

--- Formula/cecil.rb
+++ Formula/cecil.rb
@@ -1,9 +1,9 @@
 class Cecil < Formula
   desc "Your content driven static site generator."
   homepage "https://cecil.app"
-  url "https://github.com/Cecilapp/Cecil/releases/download/7.32.3/cecil.phar"
-  version "7.32.3"
-  sha256 "4b40868b4b0e5cbb4cd3cabbd7c1186b331312ff9a40a0cfbdd7efde99290624"
+  url "https://github.com/Cecilapp/Cecil/releases/download/7.32.4/cecil.phar"
+  version "7.32.4"
+  sha256 "87d34643eacc013c3755ee211ce00fc87ddb0250502911446bb9cac2328131ad"
   license "MIT"
 
   depends_on "php"
