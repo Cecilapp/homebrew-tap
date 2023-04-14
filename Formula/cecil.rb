59d9663ac68f6a04a51a30173444abf63981dd30
runner <runner@Mac-1681514043917.local>
2023-04-14 23:33:07 +0000

    cecil 7.36.0

--- Formula/cecil.rb
+++ Formula/cecil.rb
@@ -1,9 +1,9 @@
 class Cecil < Formula
   desc "Your content driven static site generator."
   homepage "https://cecil.app"
-  url "https://github.com/Cecilapp/Cecil/releases/download/7.35.2/cecil.phar"
-  version "7.35.2"
-  sha256 "92c9e61ac75bd031344ade9fc961bdde1bd29c776f5b523852acc70a20ed3657"
+  url "https://github.com/Cecilapp/Cecil/releases/download/7.36.0/cecil.phar"
+  version "7.36.0"
+  sha256 "a6aae5599176c921f4caadc505a568ec5ff3590fd96dfe57691c29ef6e3a8f51"
   license "MIT"
 
   depends_on "php"
