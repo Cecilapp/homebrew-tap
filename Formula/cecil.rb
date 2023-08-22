fff1e0d966e4b3ea4ad6fcba6419e5f01c9827f6
Arnaud Ligny <arnaud@ligny.fr>
2023-08-22 10:31:34 +0200

    cecil 7.46.6 (#80)
    Co-authored-by: runner <runner@Mac-1692616044033.local>

--- Formula/cecil.rb
+++ Formula/cecil.rb
@@ -1,9 +1,9 @@
 class Cecil < Formula
   desc "Your content driven static site generator."
   homepage "https://cecil.app"
-  url "https://github.com/Cecilapp/Cecil/releases/download/7.46.5/cecil.phar"
-  version "7.46.5"
-  sha256 "6c9756a6743886d2115ab8c86f592aae01d78356aacb857f41e33c4c08b0b3c9"
+  url "https://github.com/Cecilapp/Cecil/releases/download/7.46.6/cecil.phar"
+  version "7.46.6"
+  sha256 "17009bb51ffc9451bd38312bad8184bc885472d9d8c6cfd837483d18ac8f11d3"
   license "MIT"
 
   depends_on "php"
