78f3c053bbe6fd7820fe2cfef8e2adde439289e4
Arnaud Ligny <arnaud@ligny.fr>
2023-11-21 12:09:31 +0100

    Update cecil.rb

--- Formula/cecil.rb
+++ Formula/cecil.rb
@@ -1,17 +1,17 @@
 class Cecil < Formula
   desc "Your content driven static site generator."
   homepage "https://cecil.app"
+  license "MIT"
 
   url "https://github.com/Cecilapp/Cecil/releases/download/7.54.1/cecil.phar"
   version "7.54.1"
   sha256 "07fa6ce4b107745c295512b67fd96a8de17d082181da13b77481978654dce95d"
 
-  license "MIT"
-
   depends_on "php"
 
   def install
     bin.install "cecil.phar" => "cecil"
+    ohai "Run `cecil` to get started\n"
   end
 
   test do
