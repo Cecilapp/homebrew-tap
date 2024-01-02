377db842302a3238c553816d936566cc5e2f80a0
runner <runner@Mac-1704200964566.local>
2024-01-02 13:10:26 +0000

    cecil 8.1.5

--- Formula/cecil.rb
+++ Formula/cecil.rb
@@ -3,9 +3,9 @@ class Cecil < Formula
   homepage "https://cecil.app"
   license "MIT"
 
-  url "https://github.com/Cecilapp/Cecil/releases/download/8.1.3/cecil.phar"
-  version "8.1.3"
-  sha256 "e989b9d3d6bfc9509bcace1a24cdd2b16ed15f6d9ac6689f28130aba602fcd87"
+  url "https://github.com/Cecilapp/Cecil/releases/download/8.1.5/cecil.phar"
+  version "8.1.5"
+  sha256 "e2cef9ab7d5f7d1f08cfec6e8af80a20869d5692734c35aa18f7a9f575cbe14c"
 
   depends_on "php"
 
