3d8b5e0e71a1e1d1435d782962eadab6fd7a0aa8
Arnaud Ligny <arnaud@ligny.fr>
2024-01-15 02:18:22 +0100

    cecil 8.5.0 (#126)
    Co-authored-by: runner <runner@Mac-1705280733686.local>

--- Formula/cecil.rb
+++ Formula/cecil.rb
@@ -3,9 +3,9 @@ class Cecil < Formula
   homepage "https://cecil.app"
   license "MIT"
 
-  url "https://github.com/Cecilapp/Cecil/releases/download/8.4.0/cecil.phar"
-  version "8.4.0"
-  sha256 "577e887cf9d326745edd1b7b6ed198cb5d82876605dc2ffe0c76faba4330b60a"
+  url "https://github.com/Cecilapp/Cecil/releases/download/8.5.0/cecil.phar"
+  version "8.5.0"
+  sha256 "a7c922036cc3e0cc7b46175286b65c270a17399af19909ab98307da5a6438097"
 
   depends_on "php"
 
