42b1e2799856452a97b11640ef06d9b7d8966d23
Arnaud Ligny <arnaud@ligny.fr>
2024-01-30 15:29:42 +0100

    Bump version

--- Formula/cecil.rb
+++ Formula/cecil.rb
@@ -3,9 +3,9 @@ class Cecil < Formula
   homepage "https://cecil.app"
   license "MIT"
 
-  url "https://github.com/Cecilapp/Cecil/releases/download/8.6.1/cecil.phar"
-  version "8.6.1"
-  sha256 "ad2f66f7ac72fa5fb47f9b0e5ee1f6d128342baf15342d7dfffe85e84177da8a"
+  url "https://github.com/Cecilapp/Cecil/releases/download/8.6.4/cecil.phar"
+  version "8.6.4"
+  sha256 "0f98f24874e8d3f93eb4daaf4a83af1b00102d14e056d8908fbda946d389afb3"
 
   depends_on "php"
 
