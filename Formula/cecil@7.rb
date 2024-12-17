8fe8b9c7711bab8b79cddaa029d1da63d1ec603c
Arnaud Ligny <arnaud@ligny.fr>
2024-12-17 03:43:00 +0100

    Update cecil@7.rb

--- Formula/cecil@7.rb
+++ Formula/cecil@7.rb
@@ -3,8 +3,8 @@ class CecilAT7 < Formula
   homepage "https://cecil.app"
   license "MIT"
 
-  url "https://github.com/Cecilapp/Cecil/releases/download/7.55.0/cecil.phar"
   version "7.55.0"
+  url "https://github.com/Cecilapp/Cecil/releases/download/7.55.0/cecil.phar"
   sha256 "4b90fd0053ab3c8dc5bfa8e3ea3fe7a73521ce8aff0a4d4dcbe40b2231bc135c"
 
   keg_only :versioned_formula
