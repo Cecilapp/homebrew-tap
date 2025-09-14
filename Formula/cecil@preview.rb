cfbf5408596793a68e7d20779840defafed2585b
Arnaud Ligny <arnaud@ligny.fr>
2025-09-15 00:19:39 +0200

    Create cecil@preview.rb

---
+++ Formula/cecil@preview.rb
@@ -0,0 +1,20 @@
+class CecilPreview < Formula
+  desc "A simple and powerful content-driven static site generator."
+  homepage "https://cecil.app"
+  license "MIT"
+
+  version "X.YY.Z"
+  url "https://github.com/Cecilapp/Cecil/releases/download/X.YY.Z/cecil.phar"
+  sha256 "xxxx"
+
+  depends_on "php"
+
+  def install
+    bin.install "cecil.phar" => "cecil"
+    ohai "Run `cecil` to get started"
+  end
+
+  test do
+    shell_output("#{bin}/cecil --version").include?(version)
+  end
+end
