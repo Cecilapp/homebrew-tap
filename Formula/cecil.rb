b3a872b03b67ba89fbb2db32ee96ebddcc451f26
Arnaud Ligny <arnaud@ligny.fr>
2026-02-02 16:58:50 +0100

    cecil 8.80.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/cecil.rb
+++ Formula/cecil.rb
@@ -3,9 +3,9 @@ class Cecil < Formula
   homepage "https://cecil.app"
   license "MIT"
 
-  version "8.79.6"
-  url "https://github.com/Cecilapp/Cecil/releases/download/8.79.6/cecil.phar"
-  sha256 "d0c0e2e426c1aa4053b04d93e462c0c4b9d78674e31730646e9c64d6d3eebd0c"
+  version "8.80.0"
+  url "https://github.com/Cecilapp/Cecil/releases/download/8.80.0/cecil.phar"
+  sha256 "6e61d8fedd773b09f85b7d4d93a2948c83b3a792b9898c67cec7574392006c45"
 
   #depends_on "php"
   uses_from_macos "php", since: :monterey
