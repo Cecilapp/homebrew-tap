5ac691deec7600c668c66eb3cae54affa01110cb
Arnaud Ligny <arnaud@ligny.fr>
2026-05-30 02:54:36 +0200

    cecil-preview 8.99.0-beta.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/cecil-preview.rb
+++ Formula/cecil-preview.rb
@@ -3,9 +3,9 @@ class CecilPreview < Formula
   homepage "https://cecil.app"
   license "MIT"
 
-  version "8.86.0-beta.8"
-  url "https://github.com/Cecilapp/Cecil/releases/download/8.86.0-beta.8/cecil.phar"
-  sha256 "905e1920a77911b72174ce2ccd4fcb9ef71bb1b4a8e99050f1e8e4d4f67474c5"
+  version "8.99.0-beta.1"
+  url "https://github.com/Cecilapp/Cecil/releases/download/8.99.0-beta.1/cecil.phar"
+  sha256 "4ab28e40aac19e8ba6d16475ab13e5bbd8a6e28520875e2548dd2681f64718c3"
 
   keg_only :versioned_formula
 
